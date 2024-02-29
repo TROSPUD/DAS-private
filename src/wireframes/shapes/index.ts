3a60ae02e5ee8492d0dc1c27c6a68b2cb99d5ab5
hongzi <hongzi@bytedance.com>
2024-02-29 15:29:08 +0800

    feat: 增加antenna和bbu两种元件

--- src/wireframes/shapes/index.ts
+++ src/wireframes/shapes/index.ts
@@ -1,15 +1,14 @@
 import { RendererService } from '@app/wireframes/model/renderer.service';
-import { Button, Heading, HorizontalLine, Image, Label, Raster, Rectangle, Shape, VerticalLine } from './dependencies';
+import { HorizontalLine, Label, Raster, Shape, Antenna, VerticalLine, Bbu } from './dependencies';
 import { AbstractControl } from './utils/abstract-control';
 
 export function registerRenderers() {
-    RendererService.addRenderer(new AbstractControl(new Button()));
-    RendererService.addRenderer(new AbstractControl(new Heading()));
     RendererService.addRenderer(new AbstractControl(new HorizontalLine()));
-    RendererService.addRenderer(new AbstractControl(new Image()));
+    RendererService.addRenderer(new AbstractControl(new VerticalLine()));
+    RendererService.addRenderer(new AbstractControl(new Antenna()));
+    RendererService.addRenderer(new AbstractControl(new Bbu()));
     RendererService.addRenderer(new AbstractControl(new Label()));
     RendererService.addRenderer(new AbstractControl(new Raster()));
-    RendererService.addRenderer(new AbstractControl(new Rectangle()));
     RendererService.addRenderer(new AbstractControl(new Shape()));
-    RendererService.addRenderer(new AbstractControl(new VerticalLine()));
+
 }
---
+++ src/wireframes/shapes/neutral/antenna.ts
@@ -0,0 +1,56 @@
+import { ConfigurableFactory, DefaultAppearance, Rect2, RenderContext, ShapePlugin, Vec2 } from '@app/wireframes/interface';
+
+const STATE = 'STATE';
+const STATE_NORMAL = 'Normal';
+const STATE_CHECKED = 'Checked';
+
+const DEFAULT_APPEARANCE = {
+    [DefaultAppearance.BACKGROUND_COLOR]: 0xbdbdbd,
+    [DefaultAppearance.FOREGROUND_COLOR]: 0xb50202,
+    [DefaultAppearance.STROKE_COLOR]: 0xb50202,
+    [DefaultAppearance.STROKE_THICKNESS]: 4,
+    [DefaultAppearance.TEXT_DISABLED]: true,
+    [STATE]: STATE_CHECKED,
+};
+
+export class Antenna implements ShapePlugin {
+    public identifier(): string {
+        return 'Antenna';
+    }
+
+    public defaultAppearance() {
+        return DEFAULT_APPEARANCE;
+    }
+
+    public defaultSize() {
+        return { x: 30, y: 30 };
+    }
+
+    public configurables(factory: ConfigurableFactory) {
+        return [
+            factory.selection(STATE, 'State', [
+                STATE_NORMAL,
+                STATE_CHECKED,
+            ]),
+        ];
+    }
+
+    public render(ctx: RenderContext) {
+        const border = ctx.shape.strokeThickness;
+
+        const radius = Math.min(ctx.rect.width, ctx.rect.height) * 0.5;
+
+        const isUnchecked = ctx.shape.getAppearance(STATE) === STATE_NORMAL;
+
+        const circleY = ctx.rect.height * 0.5;
+        const circleX = isUnchecked ? radius : ctx.rect.width - radius;
+
+        const circleCenter = new Vec2(circleX, circleY);
+        const circleSize = radius - border;
+
+        // Circle.
+        ctx.renderer2.ellipse(0, Rect2.fromCenter(circleCenter, circleSize), p => {
+            p.setBackgroundColor(ctx.shape.strokeColor);
+        });
+    }
+}
